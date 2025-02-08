ac4b93c755cdf6d214d24334aa03a7f4dab1fe92
DmitriyViatkin <viatkindima@gmail.com>
2025-02-08 14:04:56 +0200

    update psrser. add writer csv and xlsx file

--- Config.py
+++ Config.py
@@ -1,3 +1,3 @@
 url_tamplat = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page="
-FILE_NAME = 'Example.xlsx'
-sheet_name='Dora'
+teg_name ='a'
+teg_description='description card-text'
--- PARSER.py
+++ PARSER.py
@@ -1,57 +1,88 @@
 import requests 
+import csv
 from bs4 import BeautifulSoup as bs
 import openpyxl as op
 
 
-from Config import url_tamplat,FILE_NAME,sheet_name
+from Config import url_tamplat,teg_name,teg_description
 
-
-
-
-def find_last_non_empty_row(file_path, sheet_name=None):
-    # Открываем Excel-файл
-    wb = op.load_workbook(file_path)
-    sheet = wb[sheet_name] if sheet_name else wb.active
-
-    # Начинаем с последней строки
-    last_row = sheet.max_row
-    while last_row > 0:
-        # Проверяем, есть ли данные в строке
-        if any(sheet.cell(row=last_row, column=col).value for col in range(1, sheet.max_column + 1)):
-            break
-        last_row -= 1  # Переходим на строку выше
-
-    wb.close()
-    return last_row
-def parser(url_tamplate,FILE_NAME,sheet_name):
+def parser(url_tamplate,teg_name,teg_description):
     
-    print(url_tamplate)
+    
     r=requests.get(url_tamplate)
     
     soup = bs(r.text, "lxml")
-    #print(soup)
+    
     produkts = soup.find_all('div', class_= 'product-wrapper card-body')
-    i=1
-    wb=op.load_workbook(FILE_NAME)
-    ws=wb.active
-    c=find_last_non_empty_row(FILE_NAME, sheet_name)
-    i=0
-    for produkt in produkts:
-        i+=1
-        
-        value1=produkt.find('a').text
-        ws['A'+str(c+i)]=value1
-        
-        
-        value2 = produkt.find(class_='description card-text').text
-        ws['B'+str(c+i)]=value2
-        
-        
-    wb.save('Example.xlsx')
+   
+    
+    laptops=[]
+    laptop={}
 
-def scrol(url_tamplate):
+    for produkt in produkts:
+        
+        laptop['name']=produkt.find(teg_name).text
+        
+        laptop['description']=produkt.find(class_= teg_description).text
+        laptops.append(laptop)
+    
+    writer_csv('Laptops_csv.csv',laptops)
+    writer_xlsx('Laptops_XLSX.xlsx',laptops)
+
+def writer_csv(filename,data):
+    headers = ['name', 'description']
+
+# Открываем файл на запись
+    with open(filename, 'a+', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, headers)
+
+        # Проверяем, пустой ли файл
+        csvfile.seek(0)
+        reader = csv.reader(csvfile)
+        try:
+            first_row = next(reader)
+            if not first_row:  # Файл пустой или только что создан
+                writer.writeheader()
+        except StopIteration:  # Файл пустой или только что создан
+            writer.writeheader()
+
+        # Перемещаем указатель в конец файла
+        csvfile.seek(0, 2)
+
+        # Записываем данные
+        writer.writerows(data)
+
+def writer_xlsx(filename, data):
+    # Создаем новый рабочий лист
+    workbook = None
+    try:
+        workbook = op.load_workbook(filename)
+        sheet = workbook.active
+    except FileNotFoundError:
+        workbook = op.Workbook()
+        sheet = workbook.active
+
+    # Определяем начальную строку для записи
+   
+    headers = ['name', 'description']
+    if not headers:
+        headers = data[0].keys()
+
+    # Записываем заголовки в первую строку
+    for col, col_name in enumerate(headers):
+        sheet.cell(row=1, column=col+1, value=col_name)
+    start_row = sheet.max_row + 1
+    # Записываем новые данные
+    for row_index, row_data in enumerate(data, start=start_row):
+        for col_index, value in enumerate(row_data.values()):
+            sheet.cell(row=row_index, column=col_index+1, value=value)
+
+    workbook.save(filename)
+
+
+def scrol(url_tamplate,teg_name,teg_description):
     for i in range(1,21):
         url=url_tamplate+str(i)
-        parser(url,FILE_NAME,sheet_name)
+        parser(url,teg_name,teg_description)
 
-scrol(url_tamplat)
+scrol(url_tamplat,teg_name,teg_description)
